ab40208687d7ee18f69771709bc09402c6a8bf75
Nic La <thinklem@Nics-MacBook-Pro.local>
2015-10-29 12:13:33 -0700

    Day of the Year

--- C_Sample/main.c
+++ C_Sample/main.c
@@ -11,33 +11,50 @@
  **********************************************************************/
 
 #include <stdio.h>
+#include <stdbool.h>
 
 int main()
 {
-    //int days_month[] = { 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31 };
+    int days_month[] = { 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31 };
     int day_in = 0;
     int month_in = 0;
     int year_in = 0;
     char line[100];
+    int i;
+    int days_year = 0;
+    bool leap_year = 0;
 
+    // Ask for date
     printf("Enter date (mm/dd/yyyy): ");
     fgets(line, sizeof(line), stdin);
     sscanf(line, "%d/%d/%d", &month_in, &day_in, &year_in);
     
+    // Determine if leap year
     if((year_in % 4) == 0){
         if((year_in % 100) == 0){
             if((year_in % 400) == 0){
-                printf("%d is a leap year\n", year_in);
+                leap_year = 1;
             } else{
-                printf("%d is NOT a leap year\n", year_in);
+                leap_year = 0;
             }
         } else{
-            printf("%d is a leap year\n", year_in);
+            leap_year = 1;
         }
     } else{
-        printf("%d is NOT a leap year\n", year_in);
+        leap_year = 0;
+    }
+    if(leap_year == 1){
+        days_month[1] = 29;
     }
     
+    // Count day of the year
+    for(i = 0; i < (month_in - 1); i++){
+        days_year += days_month[i];
+    }
+    days_year += day_in;
+    
+    
+    printf("Day of the year = %d\n", days_year);
 }
 
 
