5ecc826cd74d89120016691c51148ebb0e443b55
Nic La <thinklem@Nics-MacBook-Pro.local>
2015-11-22 19:59:32 -0800

    Text File Character Count

--- C_Sample/main.c
+++ C_Sample/main.c
@@ -1,61 +1,41 @@
 /**********************************************************************
- * Title: Phone Number
+ * Title: Character Count
  *
  * Author: Nic La
  *
- * Often times in commercials, phone numbers contain letters so that they're 
- * easy to remember (e.g. 1-800-VERIZON). Write a program that will convert a 
- * phone number that contains letters into a phone number with only numbers and 
- * the appropriate dash. Click here to learn more about the telephone keypad.
- *
- * Example Execution: Input: 1-800-COMCAST Output: 1-800-266-2278
- *
- * Link: https://www.reddit.com/r/dailyprogrammer/comments/qit0h/352012_challenge_18_easy/
- *
- * Warning: Simplified code expects format #-###-LLLLLLL.
+ * Purpose: To count the number of characters inside a text file.
  *
  **********************************************************************/
 
 
 #include <stdio.h>
-#include <string.h>
+const char FILE_NAME[] = "/Users/thinklem/Dropbox/Embedded Systems/C_Sample/C_Sample/input.txt";
+#include <stdlib.h>
 
-int main()
-{
-    char line[100];
-    int i;
-    int j;
-    int k;
-    char letterpad[8][4] = {
-        {'A', 'B', 'C'}, {'D', 'E', 'F'}, {'G', 'H', 'I'}, {'J', 'K', 'L'},
-        {'M', 'N', 'O'}, {'P', 'Q', 'R', 'S'}, {'T', 'U', 'V'}, {'W', 'X', 'Y', 'Z'}
-    };
-    char keypad[8] = {'2', '3', '4', '5', '6', '7', '8', '9'};
-    int count = 0;
+int main(){
+    int count = 0;          /* number of characters seen */
+    FILE *in_file;          /* input file */
     
-    printf("Enter phone number: ");
-    fgets(line, sizeof(line), stdin);
-    line[strlen(line)-1] = '\0';
+    /* character of EOF flag from input */
+    int ch;
     
-    for(i = 0; i < strlen(line); i++){
-        if((line[i] > 64) && (line[i] < 91)){
-            for(j = 0; j < 8; j++){
-                for(k = 0; k < 4; k++){
-                    if(line[i] == letterpad[j][k]){
-                        printf("%c", keypad[j]);
-                        break;
-                    }
-                }
-            }
-            
-            count++;
-            if(count == 3){
-                printf("-");
-            }
-        } else{
-            printf("%c", line[i]);
-        }
+    in_file = fopen(FILE_NAME, "r");
+    if(in_file == NULL) {
+        printf("Cannot open %s\n", FILE_NAME);
+        exit(8);
     }
+    
+    while(1){
+        ch = fgetc(in_file);
+        if(ch == EOF){
+            break;
+        }
+        ++count;
+    }
+    printf("Number of characters in %s is %d\n", FILE_NAME, count);
+    
+    fclose(in_file);
+    return(0);
 }
 
 
