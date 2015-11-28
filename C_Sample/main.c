adb494efe0e05940bade8785223fc5f23436fc0d
Nic La <thinklem@nics-mbp.attlocal.net>
2015-11-27 18:45:31 -0800

    Compare Two Lists

--- C_Sample/main.c
+++ C_Sample/main.c
@@ -3,89 +3,48 @@
  *
  * Author: Nic La
  *
- * Purpose: Input: a number
- * Output : the next higher number that uses the same set of digits.
- * 
- * Example: Input: 429
- *          Output: 492
+ * Purpose: Write a program that will compare two lists, and append any 
+ * elements in the second list that doesn't exist in the first.
+ * input: ["a","b","c",1,4,], ["a", "x", 34, 4]
+ * output: ["a", "b", "c",1,4,"x",34]
  *
- * Link: https://www.reddit.com/r/dailyprogrammer/comments/qp3ub/392012_challenge_21_easy/
- *
- * permute function adapted from http://www.geeksforgeeks.org/write-a-c-program-to-print-all-permutations-of-a-given-string/
+ * Link: https://www.reddit.com/r/dailyprogrammer/comments/qr0hg/3102012_challenge_22_easy/
  *
  **********************************************************************/
 
 
 #include <stdio.h>
 #include <string.h>
-#include <stdlib.h>
 
-int num_array[100];
-int greater_array[100];
-int count = 0;
+const int list_length1 = 5;
+const int list_length2 = 4;
 
-/* Function to swap values at two pointers */
-void swap(char *x, char *y)
-{
-    char temp;
-    temp = *x;
-    *x = *y;
-    *y = temp;
-}
-
-/* Function to print permutations of string
- This function takes three parameters:
- 1. String
- 2. Starting index of the string
- 3. Ending index of the string. */
-void permute(char *a, int l, int r)
-{
-    int i;
-    
-    if (l == r){
-        num_array[count] = atoi(a);
-        count++;
-    } else{
-        for (i = l; i <= r; i++){
-            swap((a+l), (a+i));
-            permute(a, l+1, r);
-            swap((a+l), (a+i)); //backtrack
-        }
-    }
-}
+char list1[list_length1][10] = {"a", "b", "c", "1", "4"};
+char list2[list_length2][10] = {"a", "x", "34", "4"};
 
 int main()
 {
-    char line[100];
-    int j, k;
-    int count2 = 0;
-    int min = 0;
+    int i, j;
+    int count = 0;
     
-    printf("Enter number: ");
-    fgets(line, sizeof(line), stdin);
-    line[strlen(line)-1] = '\0';
-    
-    int n = (int)strlen(line);
-    permute(line, 0, n-1);
-    
-    // Take numbers greater than num_array[0]
-    // Store numbers under greater_array
-    // Of these numbers print the lowest
-    for(j = 0; j < count; j++){
-        if(num_array[j] > num_array[0]){
-            greater_array[count2] = num_array[j];
-            count2++;
+    for(i = 0; i < list_length2; i++){
+        for(j = 0; j < list_length1; j++){
+            if(i == 0){
+                printf("[%s]", list1[j]);
+            }
+            
+            if(strcmp(list2[i], list1[j]) == 0){
+                count++;
+            }
         }
+        if(count == 0){
+            printf("[%s]", list2[i]);
+        }
+        count = 0;
     }
     
-    min = greater_array[0];
     
-    for(k = 0; k < count2; k++){
-        if(greater_array[k] < min){
-            min = greater_array[k];
-        }
-    }
-    printf("%d\n", min);
+    
 }
 
 
