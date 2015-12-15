9be1cd28aa8dc128f1c90ef4ff383ee33fe2a343
Nic La <thinklem@nics-mbp.attlocal.net>
2015-12-15 15:54:58 -0800

    Duplicates

--- C_Sample/main.c
+++ C_Sample/main.c
@@ -1,56 +1,68 @@
 /**********************************************************************
- * Title: Majority Wins
+ * Title: Duplicates
  *
  * Author: Nic La
  *
- * Purpose: In an election, the person with the majority of the votes is 
- * the winner. Sometimes due to similar number of votes, there are no winners.
- * Your challenge is to write a program that determines the winner of a vote, 
- * or shows that there are no winners due to a lack of majority.
- * 
- * Detail: Lack of majority means no 50% of the voters agree on a single candidate
- *
- * Link: https://www.reddit.com/r/dailyprogrammer/comments/qxuug/3152012_challenge_25_easy/
+ * Purpose: you have a string "ddaaiillyypprrooggrraammeerr". We want to 
+ * remove all the duplicates and put them in a separate string.
  *
  **********************************************************************/
 
 #include <stdio.h>
+#include <string.h>
 
-int main(){
-    char line[100];
-    int num_candidates = 0;
-    int indv_votes[100];
-    int total_votes = 0;
-    int winner = 0;
+char *remove_char(char *string_ptr, char duplicate){
+    char *temp_string_ptr = string_ptr;
+    char string[100];
+    int count = 0;
     
-    // Ask for number of candidates
-    printf("Enter number of candidates: ");
-    fgets(line, sizeof(line), stdin);
-    sscanf(line, "%d", &num_candidates);
-    
-    // Ask for number of votes for each candidate
-    for(int i = 1; i <= num_candidates; i++){
-        printf("Enter votes candidate %d received: ", i);
-        fgets(line, sizeof(line), stdin);
-        sscanf(line, "%d", &indv_votes[i]);
-        total_votes += indv_votes[i];
+    for(int i = 0; i < 2; i++){
+        while(*string_ptr != duplicate){
+            if(*string_ptr == '\0'){
+                return(NULL);
+            }
+            string_ptr++;
+            count++;
+        }
+        string_ptr++;
+        count++;
     }
     
-    // Determine if any candidate received a majority
-    for(int j = 1; j <= num_candidates; j++){
-        if(indv_votes[j] > (total_votes / 2)){
-            winner = j;
-            break;
+    temp_string_ptr[count - 1] = '\0';
+    strcpy(string, string_ptr);
+    strcat(temp_string_ptr, string);
+    
+    return(temp_string_ptr);
+}
+
+int main(){
+    int i, j;
+    int count = 0;
+    char string1[100];
+    char string2[100];
+    char temp_string[100];
+    
+    printf("Enter string: ");
+    fgets(string1, sizeof(string1), stdin);
+    string1[strlen(string1) - 1] = '\0';
+    
+    for(i = 0; i < strlen(string1); i++){
+        for(j = 0; j < strlen(string1); j++){
+            if((string1[i] == string1[j]) && (i != j)){
+                // Copy duplicate char into string2[]
+                string2[count] = string1[j];
+                
+                // Remove duplicate char from string1[]
+                strcpy(temp_string, remove_char(string1, string2[count]));
+                strcpy(string1, temp_string);
+                count++;
+            }
         }
     }
     
-    // Print the winner
-    if(winner != 0){
-        printf("The winner is candidate %d!\n", winner);
-    } else{
-        printf("There is no winner due to lack of majority\n");
-    }
+    printf("%s\n%s\n", string1, string2);
 }
+
 
 
 
