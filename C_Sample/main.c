a0c61605814f9e33e8908cd3c3a0f9eee37edfd6
Nic La <thinklem@nics-mbp.attlocal.net>
2015-12-16 12:52:08 -0800

    Leap Year

--- C_Sample/main.c
+++ C_Sample/main.c
@@ -1,66 +1,69 @@
 /**********************************************************************
- * Title: Duplicates
+ * Title: Leap Year
  *
  * Author: Nic La
  *
- * Purpose: you have a string "ddaaiillyypprrooggrraammeerr". We want to 
- * remove all the duplicates and put them in a separate string.
+ * Purpose: Write a program that accepts a year as input and outputs the 
+ * century the year belongs in (e.g. 18th century's year ranges are 1701 
+ * to 1800) and whether or not the year is a leap year. Pseudocode for 
+ * leap year can be found here.
+ *
+ * Sample run:
+ * Enter Year: 1996
+ * Century: 20
+ * Leap Year: Yes
+ * Enter Year: 1900
+ * Century: 19
+ * Leap Year: No
+ *
+ * Link: https://www.reddit.com/r/dailyprogrammer/comments/r0r3h/3172012_challenge_27_easy/
  *
  **********************************************************************/
 
 #include <stdio.h>
-#include <string.h>
+#include <stdbool.h>
 
-char *remove_char(char *string_ptr, char duplicate){
-    char *temp_string_ptr = string_ptr;
-    char string[100];
-    int count = 0;
+int main()
+{
+    char line[100];
+    int year_in = 0;
+    bool leap_year = 0;
+    int century = 0;
     
-    for(int i = 0; i < 2; i++){
-        while(*string_ptr != duplicate){
-            if(*string_ptr == '\0'){
-                return(NULL);
-            }
-            string_ptr++;
-            count++;
-        }
-        string_ptr++;
-        count++;
+    // Ask for date
+    printf("Enter Year: ");
+    fgets(line, sizeof(line), stdin);
+    sscanf(line, "%d", &year_in);
+    
+    // Determine century
+    if((year_in % 100) == 0){
+        century = year_in / 100;
+    } else{
+        century = (year_in / 100) + 1;
     }
     
-    temp_string_ptr[count - 1] = '\0';
-    strcpy(string, string_ptr);
-    strcat(temp_string_ptr, string);
-    
-    return(temp_string_ptr);
-}
-
-int main(){
-    int i, j;
-    int count = 0;
-    char string1[100];
-    char string2[100];
-    char temp_string[100];
-    
-    printf("Enter string: ");
-    fgets(string1, sizeof(string1), stdin);
-    string1[strlen(string1) - 1] = '\0';
-    
-    for(i = 0; i < strlen(string1); i++){
-        for(j = 0; j < strlen(string1); j++){
-            if((string1[i] == string1[j]) && (i != j)){
-                // Copy duplicate char into string2[]
-                string2[count] = string1[j];
-                
-                // Remove duplicate char from string1[]
-                strcpy(temp_string, remove_char(string1, string2[count]));
-                strcpy(string1, temp_string);
-                count++;
+    // Determine if leap year
+    if((year_in % 4) == 0){
+        if((year_in % 100) == 0){
+            if((year_in % 400) == 0){
+                leap_year = 1;
+            } else{
+                leap_year = 0;
             }
+        } else{
+            leap_year = 1;
         }
+    } else{
+        leap_year = 0;
     }
     
-    printf("%s\n%s\n", string1, string2);
+    // Print
+    printf("Century: %d\n", century);
+    if(leap_year == 1){
+        printf("Leap Year: Yes\n");
+    } else{
+        printf("Leap Year: No\n");
+    }
 }
 
 
