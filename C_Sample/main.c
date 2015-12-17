501c8cb00f662b3de19d52990f6bef6d2e4d1154
Nic La <thinklem@nics-mbp.attlocal.net>
2015-12-17 11:17:24 -0800

    Array Duplicates

--- C_Sample/main.c
+++ C_Sample/main.c
@@ -1,68 +1,53 @@
 /**********************************************************************
- * Title: Leap Year
+ * Title: Array Duplicates
  *
  * Author: Nic La
  *
- * Purpose: Write a program that accepts a year as input and outputs the 
- * century the year belongs in (e.g. 18th century's year ranges are 1701 
- * to 1800) and whether or not the year is a leap year. Pseudocode for 
- * leap year can be found here.
+ * Purpose: The array duplicates problem is when one integer is in an array 
+ * for more than once. If you are given an array with integers between 1 and 
+ * 1,000,000 or in some other interval and one integer is in the array twice. 
+ * How can you determine which one? Your task is to write code to solve the 
+ * challenge.
  *
- * Sample run:
- * Enter Year: 1996
- * Century: 20
- * Leap Year: Yes
- * Enter Year: 1900
- * Century: 19
- * Leap Year: No
+ * Note: try to find the most efficient way to solve this challenge.
  *
- * Link: https://www.reddit.com/r/dailyprogrammer/comments/r0r3h/3172012_challenge_27_easy/
+ * Link: https://www.reddit.com/r/dailyprogrammer/comments/r59kk/3202012_challenge_28_easy/
  *
  **********************************************************************/
 
+#define SIZE 10
+
 #include <stdio.h>
-#include <stdbool.h>
+
 
 int main()
 {
-    char line[100];
-    int year_in = 0;
-    bool leap_year = 0;
-    int century = 0;
+    int original_list[SIZE] = { 8, 9, 7, 5, 7, 4, 3, 2, 1, 6 };
+    int new_list[SIZE];
+    int i, j, k, m;
     
-    // Ask for date
-    printf("Enter Year: ");
-    fgets(line, sizeof(line), stdin);
-    sscanf(line, "%d", &year_in);
-    
-    // Determine century
-    if((year_in % 100) == 0){
-        century = year_in / 100;
-    } else{
-        century = (year_in / 100) + 1;
+    // Store list in new array
+    for(i = 0; i < SIZE; i++){
+        new_list[i] = original_list[i];
     }
     
-    // Determine if leap year
-    if((year_in % 4) == 0){
-        if((year_in % 100) == 0){
-            if((year_in % 400) == 0){
-                leap_year = 1;
-            } else{
-                leap_year = 0;
+    // Sort new array in increasing order
+    for(j = 0; j < SIZE; j++){
+        for(k = 0; k < SIZE; k++){
+            if(new_list[j] < new_list[k]){
+                new_list[j] += new_list[k];
+                new_list[k] = new_list[j] - new_list[k];
+                new_list[j] -= new_list[k];
             }
-        } else{
-            leap_year = 1;
         }
-    } else{
-        leap_year = 0;
     }
     
-    // Print
-    printf("Century: %d\n", century);
-    if(leap_year == 1){
-        printf("Leap Year: Yes\n");
-    } else{
-        printf("Leap Year: No\n");
+    // Compare adjacent elements
+    // If equal, print duplicates
+    for(m = 0; m < SIZE; m++){
+        if(new_list[m] == new_list[m+1]){
+            printf("[%d]", new_list[m]);
+        }
     }
 }
 
