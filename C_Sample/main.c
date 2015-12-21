50127d5273d0107a8cdc1aafd5c3deea1ce47c3d
Nic La <thinklem@nics-mbp.attlocal.net>
2015-12-21 10:45:19 -0800

    Base 26

--- C_Sample/main.c
+++ C_Sample/main.c
@@ -1,40 +1,85 @@
 /**********************************************************************
- * Title: Target Number
+ * Title: Base 26
  *
  * Author: Nic La
  *
- * Purpose: Write a program that takes a list of integers and a target number 
- * and determines if any two integers in the list sum to the target number. 
- * If so, return the two numbers. If not, return an indication that no such 
- * integers exist.
+ * Purpose: Write a function that takes two base-26 numbers in which digits 
+ * are represented by letters with A=0, B=1, … Z=25 and returns their product 
+ * using the same notation. As an example, CSGHJ × CBA = FNEUZJA. Your task 
+ * is to write the base-26 multiplication function. Try to be very efficient 
+ * in your code!
  *
- * Link: https://www.reddit.com/r/dailyprogrammer/comments/reago/3262012_challenge_30_easy/
+ * Link: https://www.reddit.com/r/dailyprogrammer/comments/rg1vv/3272012_challenge_31_easy/
  *
  **********************************************************************/
 
 #include <stdio.h>
-#include <stdbool.h>
-
-#define SIZE 12
+#include <string.h> /* For string functions */
+#include <math.h> /* For pow function */
 
 int main()
 {
-    int num_list[SIZE] = { 1, 2, 3, 4, 5, 6, 34, 24, 15, 8, 13, 7 };
-    int target_num = 42;
-    int i, j;
-    bool exist = 0;
+    char line[100];
+    int temp = 0;
+    int decimal1 = 0;
+    int decimal2 = 0;
+    int multiple = 0;
+    int invert[100];
+    int count = 0;
+    int i;
     
-    for(i = 0; i < SIZE; i++){
-        for(j = 0; j < SIZE; j++){
-            if(((num_list[i] + num_list[j]) == target_num) && (i != j)){
-                printf("%d, %d\n", num_list[i], num_list[j]);
-                exist = 1;
-            }
+    // Store first base-26 number
+    printf("Enter first base-26 number: ");
+    fgets(line, sizeof(line), stdin);
+    line[strlen(line) - 1] = '\0';
+    
+    // Necessary because strlen(line) changes inside the for loop
+    temp = (int)strlen(line);
+    
+    for(i = 0; i < temp; i++){
+        // If lowercase, make uppercase
+        if((line[i] >= 97) && (line[i] <= 122)){
+            line[i] -= 32;
         }
+        
+        // Convert to integer
+        line[i] -= 65;
+        
+        // Convert to decimal
+        decimal1 += line[i] * pow(26, temp - 1 - i);
     }
     
-    if(exist == 0){
-        printf("No such integers exist\n");
+    // Store second base-26 number
+    printf("Enter second base-26 number: ");
+    fgets(line, sizeof(line), stdin);
+    line[strlen(line) - 1] = '\0';
+    temp = (int)strlen(line);
+    for(i = 0; i < temp; i++){
+        if((line[i] >= 97) && (line[i] <= 122)){
+            line[i] -= 32;
+        }
+        line[i] -= 65;
+        decimal2 += line[i] * pow(26, temp - 1 - i);
+    }
+    
+    // Perform decimal multiplication
+    multiple = decimal1 * decimal2;
+    
+    // Convert result to base-26
+    // Store backwards in int array
+    while(multiple != 0){
+        invert[count] = multiple % 26;
+        multiple /= 26;
+        count++;
+    }
+    
+    printf("Multiple = ");
+    // Represent result as base-26
+    // Print array in correct order
+    for(i = count; i > 0; i--){
+        invert[i - 1] += 65;
+        printf("%c", invert[i - 1]);
+        
     }
 
 }
