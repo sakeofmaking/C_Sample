b9fb7bd73c1556496d7e452c4d3769f31e8513ef
Nic La <thinklem@nics-mbp.attlocal.net>
2015-12-26 19:45:28 -0800

    QnA

--- C_Sample/main.c
+++ C_Sample/main.c
@@ -1,88 +1,124 @@
 /**********************************************************************
- * Title: Base 26
+ * Title: QnA
  *
  * Author: Nic La
  *
- * Purpose: Write a function that takes two base-26 numbers in which digits 
- * are represented by letters with A=0, B=1, … Z=25 and returns their product 
- * using the same notation. As an example, CSGHJ × CBA = FNEUZJA. Your task 
- * is to write the base-26 multiplication function. Try to be very efficient 
- * in your code!
+ * Purpose: Write a program that prints a string from a list at random, 
+ * expects input, checks for a right or wrong answer, and keeps doing it 
+ * until the user types "exit". If given the right answer for the string 
+ * printed, it will print another and continue on. If the answer is wrong, 
+ * the correct answer is printed and the program continues.
  *
- * Link: https://www.reddit.com/r/dailyprogrammer/comments/rg1vv/3272012_challenge_31_easy/
+ * Bonus: Instead of defining the values in the program, the questions/answers 
+ * is in a file, formatted for easy parsing.
+ *
+ * Example file:
+ * 12 * 12?,144
+ * What is reddit?,website with cats
+ * Translate: hola,hello
+ *
+ * Link: https://www.reddit.com/r/dailyprogrammer/comments/rl24e/3302012_challenge_33_easy/
  *
  **********************************************************************/
 
 #include <stdio.h>
-#include <string.h> /* For string functions */
-#include <math.h> /* For pow function */
+#include <string.h>                 /* For string functions */
+#include <stdlib.h>                 /* For random number generation */
+#include <time.h>                   /* For seeding random number */
+#include <stdbool.h>                /* For boolean variables */
 
-int main()
-{
-    char line[100];
-    int temp = 0;
-    int decimal1 = 0;
-    int decimal2 = 0;
-    int multiple = 0;
-    int invert[100];
-    int count = 0;
+const char QNA_LIST[] = "/Users/thinklem/Dropbox/Embedded Systems/C_Sample/C_Sample/QnA.txt";
+
+// Generate random number within range
+int randRange(int low, int high){
+    return rand() % (high - low + 1) + low;
+}
+
+int main(){
+    char line[100];                 /* Store input from user */
+    char question[100];             /* Store individual Q from QnA list */
+    char answer[100];               /* Store individual A from QnA list */
+    int count = 0;                  /* Increment for new line in QnA list */
+    bool ans = 0;
+    int rand_num = 0;               /* Store random number */
+    FILE *qna_list_ptr;             /* Input file */
     int i;
     
-    // Store first base-26 number
-    printf("Enter first base-26 number: ");
-    fgets(line, sizeof(line), stdin);
-    line[strlen(line) - 1] = '\0';
+    srand((int)time(NULL));         /* Seed random number */
     
-    // Necessary because strlen(line) changes inside the for loop
-    temp = (int)strlen(line);
-    
-    for(i = 0; i < temp; i++){
-        // If lowercase, make uppercase
-        if((line[i] >= 97) && (line[i] <= 122)){
-            line[i] -= 32;
+    while(1){
+        // Print random question from text file
+        // Open text file for reading
+        qna_list_ptr = fopen(QNA_LIST, "r");
+        if(qna_list_ptr == NULL) {
+            printf("Cannot open %s\n", QNA_LIST);
+            fclose(qna_list_ptr);
+            exit(8);
         }
         
-        // Convert to integer
-        line[i] -= 65;
-        
-        // Convert to decimal
-        decimal1 += line[i] * pow(26, temp - 1 - i);
-    }
-    
-    // Store second base-26 number
-    printf("Enter second base-26 number: ");
-    fgets(line, sizeof(line), stdin);
-    line[strlen(line) - 1] = '\0';
-    temp = (int)strlen(line);
-    for(i = 0; i < temp; i++){
-        if((line[i] >= 97) && (line[i] <= 122)){
-            line[i] -= 32;
+        // Counts number of lines in text file
+        count = 0;
+        while(fgets(line, sizeof(line), qna_list_ptr)){
+            count++;
         }
-        line[i] -= 65;
-        decimal2 += line[i] * pow(26, temp - 1 - i);
-    }
+        
+        // Generate random number
+        rand_num = randRange(1, count);
+        
+        fclose(qna_list_ptr);
+        
+        // Open text file for reading
+        qna_list_ptr = fopen(QNA_LIST, "r");
+        if(qna_list_ptr == NULL) {
+            printf("Cannot open %s\n", QNA_LIST);
+            fclose(qna_list_ptr);
+            exit(8);
+        }
+        
+        // Print Q
+        count = 1;
+        while(fgets(line, sizeof(line), qna_list_ptr) && (rand_num != count)){
+            count++;
+        }
+        fclose(qna_list_ptr);
+        
+        // Separate line into Q and A
+        ans = 0;
+        count = 0;
+        for(i = 0; i < strlen(line); i++){
+            if((line[i] != '?') && (ans == 0)){
+                question[i] = line[i];
+            } else if(line[i] == '?'){
+                question[i] = line[i];
+                question[i + 1] = '\0';
+                ans = 1;
+            } else if(ans == 1){
+                answer[count] = line[i];
+                count++;
+            }
+        }
+        answer[count - 1] = '\0';
+        printf("%s\n", question);
     
-    // Perform decimal multiplication
-    multiple = decimal1 * decimal2;
-    
-    // Convert result to base-26
-    // Store backwards in int array
-    while(multiple != 0){
-        invert[count] = multiple % 26;
-        multiple /= 26;
-        count++;
-    }
-    
-    printf("Multiple = ");
-    // Represent result as base-26
-    // Print array in correct order
-    for(i = count; i > 0; i--){
-        invert[i - 1] += 65;
-        printf("%c", invert[i - 1]);
+        
+        // Accept input
+        fgets(line, sizeof(line), stdin);
+        line[strlen(line) - 1] = '\0';
+        
+        // Check for exit
+        if(strcmp(line, "exit") == 0){
+            exit(8);
+        }
+        
+        // Compare input with answer
+        if(strcmp(line, answer) != 0){
+            printf("%s\n", answer);
+        }
         
     }
-
+  
 }
+
 
 
 
