6fdf36d0f24577945bbae0f9003fb45b37d2d302
Nic La <thinklem@Nics-MacBook-Pro.local>
2016-01-09 14:19:00 -0800

    FizzBuzz

--- C_Sample/main.c
+++ C_Sample/main.c
@@ -1,155 +1,48 @@
 /**********************************************************************
- * Title: Count File Lines
+ * Title: FizzBuzz
  *
  * Author: Nic La
  *
- * Purpose: write a program that takes
- * input : a file as an argument
- * output: counts the total number of lines.
- * for bonus, also count the number of words in the file.
+ * Purpose: You are to write a function that displays the numbers from 1 
+ * to an input parameter n, one per line, except that if the current number 
+ * is divisible by 3 the function should write “Fizz” instead of the number, 
+ * if the current number is divisible by 5 the function should write “Buzz” 
+ * instead of the number, and if the current number is divisible by both 3 
+ * and 5 the function should write “FizzBuzz” instead of the number.
  *
- * Link: https://www.reddit.com/r/dailyprogrammer/comments/rzdwq/482012_challenge_37_easy/
+ * For instance, if n is 20, the program should write 1, 2, Fizz, 4, Buzz, 
+ * Fizz, 7, 8, Fizz, Buzz, 11, Fizz, 13, 14, FizzBuzz, 16, 17, Fizz, 19, 
+ * and Buzz on twenty successive lines. 
+ *
+ * Link: https://www.reddit.com/r/dailyprogrammer/comments/s6bas/4122012_challenge_39_easy/
  *
  **********************************************************************/
 
 #include <stdio.h>
-#include <string.h>
-#include <stdlib.h>
 
-const char IN_FILE[] = "/Users/thinklem/Dropbox/Embedded Systems/C_Sample/C_Sample/input.txt";
-
-int main(){
-    FILE *in_file_ptr;             // Input file
-    char line[100];
-    int line_count = 0;
-    int word_count = 0;
+int main()
+{
+    char line[100];     // Store input line
+    int n = 0;          // Store input parameter
     int i;
     
-    // Open text file for reading
-    in_file_ptr = fopen(IN_FILE, "r");
-    if(in_file_ptr == NULL) {
-        printf("Cannot open %s\n", IN_FILE);
-        fclose(in_file_ptr);
-        exit(8);
+    printf("Enter number: ");
+    fgets(line, sizeof(line), stdin);
+    sscanf(line, "%d", &n);
+    
+    for(i = 1; i <= n; i++){
+        if(i%3 == 0){
+            printf("Fizz");
+        }
+        if(i%5 == 0){
+            printf("Buzz");
+        }
+        if((i%3 != 0) && (i%5 != 0)){
+            printf("%d", i);
+        }
+        printf("\n");
     }
     
-    // Counts number of lines and words in text file
-    // Assumption is that a space is between each word
-    while(fgets(line, sizeof(line), in_file_ptr)){
-        line_count++;
-        for(i = 0; i < strlen(line); i++){
-            if(line[i] == ' '){
-                word_count++;
-            }
-        }
-    }
-    word_count += line_count;
-    
-    // Print number of lines in text file
-    printf("Number of lines in file: %d\n", line_count);
-    
-    // Print number of words in text file
-    printf("Number of words: %d\n", word_count);
-    
-    // Close text file
-    fclose(in_file_ptr);
-    return(0);
-    
-    
-    
-
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
- /*
-    char line[100];                 //Store input from user
-    char question[100];             / Store individual Q from QnA list
-    char answer[100];               / Store individual A from QnA list
-    int count = 0;                  / Increment for new line in QnA list
-    bool ans = 0;
-    int rand_num = 0;               / Store random number
-    FILE *qna_list_ptr;             / Input file
-    int i;
-    
-    while(1){
-        // Print random question from text file
-        // Open text file for reading
-        qna_list_ptr = fopen(IN_FILE, "r");
-        if(qna_list_ptr == NULL) {
-            printf("Cannot open %s\n", IN_FILE);
-            fclose(qna_list_ptr);
-            exit(8);
-        }
-        
-        // Counts number of lines in text file
-        count = 0;
-        while(fgets(line, sizeof(line), qna_list_ptr)){
-            count++;
-        }
-        
-        // Generate random number
-        rand_num = randRange(1, count);
-        
-        fclose(qna_list_ptr);
-        
-        // Open text file for reading
-        qna_list_ptr = fopen(IN_FILE, "r");
-        if(qna_list_ptr == NULL) {
-            printf("Cannot open %s\n", IN_FILE);
-            fclose(qna_list_ptr);
-            exit(8);
-        }
-        
-        // Print Q
-        count = 1;
-        while(fgets(line, sizeof(line), qna_list_ptr) && (rand_num != count)){
-            count++;
-        }
-        fclose(qna_list_ptr);
-        
-        // Separate line into Q and A
-        ans = 0;
-        count = 0;
-        for(i = 0; i < strlen(line); i++){
-            if((line[i] != '?') && (ans == 0)){
-                question[i] = line[i];
-            } else if(line[i] == '?'){
-                question[i] = line[i];
-                question[i + 1] = '\0';
-                ans = 1;
-            } else if(ans == 1){
-                answer[count] = line[i];
-                count++;
-            }
-        }
-        answer[count - 1] = '\0';
-        printf("%s\n", question);
-        
-        
-        // Accept input
-        fgets(line, sizeof(line), stdin);
-        line[strlen(line) - 1] = '\0';
-        
-        // Check for exit
-        if(strcmp(line, "exit") == 0){
-            exit(8);
-        }
-        
-        // Compare input with answer
-        if(strcmp(line, answer) != 0){
-            printf("%s\n", answer);
-        }
-        
-    }
-    */
 }
 
 
